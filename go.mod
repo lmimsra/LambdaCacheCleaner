f5ee2d02a0208032851c37f6aa9924fe881fffe7
Lima <lmimsra07@gmail.com>
2020-02-11 22:21:29 +0900

    slackの疎通確認

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module cloudFrontInvalidation
 
 go 1.13
+
+require (
+	github.com/ashwanthkumar/slack-go-webhook v0.0.0-20200209025033-430dd4e66960
+	github.com/elazarl/goproxy v0.0.0-20191011121108-aa519ddbe484 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/parnurzeal/gorequest v0.2.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"github.com/ashwanthkumar/slack-go-webhook"
+	"github.com/joho/godotenv"
+	"os"
+)
 
 func main() {
-	fmt.Print("hello")
+	// 後でLambda化
+	_, _ = handle()
+}
+
+func handle() (string, error) {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		fmt.Println("[ERROR] env load error")
+		return "process error!", errors.New("can not find env file")
+	}
+
+	slackPostErr1 := postSlack("キャッシュ削除開始します", true)
+	if slackPostErr1 != nil {
+		return "process not complete...", slackPostErr1
+	}
+
+	invalidateResult := doInvalidate()
+
+	slackPostErr2 := postSlack("", invalidateResult)
+	if slackPostErr2 != nil {
+		return "process not complete...", slackPostErr2
+	}
+
+	return "process complete!", nil
+}
+
+// キャッシュ削除処理
+// 削除完了は待てないので、実行だけ。
+func doInvalidate() bool {
+	return false
+}
+
+// slackに連絡入れる
+func postSlack(message string, status bool) error {
+	webHookURL := os.Getenv("SLACK_WEB_HOOK_URL")
+	iconUrl := os.Getenv("SLACK_ICON_URL")
+	if len(message) == 0 {
+		message = map[bool]string{
+			true:  "キャッシュ削除中\nそのうち削除されるのでしばしお待ちを",
+			false: "キャッシュ削除失敗\nログを確認してください",
+		}[status]
+	}
+
+	field := slack.Field{Title: "CloudFront", Value: message}
+	attachment := slack.Attachment{}
+	attachment.AddField(field)
+	color := map[bool]string{true: "good", false: "danger"}[status]
+	attachment.Color = &color
+	payload := slack.Payload{
+		Parse:       "",
+		Username:    "aws-message",
+		IconUrl:     iconUrl,
+		IconEmoji:   "",
+		Channel:     "",
+		Text:        "",
+		LinkNames:   "",
+		Attachments: []slack.Attachment{attachment},
+		UnfurlLinks: false,
+		UnfurlMedia: false,
+		Markdown:    false,
+	}
+	err := slack.Send(webHookURL, "", payload)
+	if err != nil {
+		fmt.Println("[ERROR] Slack post fail")
+		return errors.New("SlackPostFail")
+	}
+	fmt.Println("[INFO] Slack post success")
+	return nil
 }

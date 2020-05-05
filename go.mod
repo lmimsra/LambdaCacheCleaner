782bb96d9dc267744bd1de0b1f4c41da415f6f9b
Lima <lmimsra07@gmail.com>
2020-05-05 17:34:07 +0900

    キャッシュ削除処理追加、残タスクのTODO追記

--- go.mod
+++ go.mod
@@ -4,11 +4,10 @@ go 1.13
 
 require (
 	github.com/ashwanthkumar/slack-go-webhook v0.0.0-20200209025033-430dd4e66960
+	github.com/aws/aws-sdk-go v1.30.20
 	github.com/elazarl/goproxy v0.0.0-20191011121108-aa519ddbe484 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/parnurzeal/gorequest v0.2.16 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"github.com/ashwanthkumar/slack-go-webhook"
+	"github.com/aws/aws-sdk-go/service/cloudfront"
 	"github.com/joho/godotenv"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -20,12 +23,21 @@ func handle() (string, error) {
 		return "process error!", errors.New("can not find env file")
 	}
 
+	// TODO Lambda化して引数追加
+	invalidateInput := makeInvalidate()
+
 	slackPostErr1 := postSlack("キャッシュ削除開始します", true)
 	if slackPostErr1 != nil {
 		return "process not complete...", slackPostErr1
 	}
 
-	invalidateResult := doInvalidate()
+	// キャッシュ削除実行
+	_, invalidateError := doInvalidate(invalidateInput)
+
+	invalidateResult := true
+	if invalidateError != nil {
+		invalidateResult = false
+	}
 
 	slackPostErr2 := postSlack("", invalidateResult)
 	if slackPostErr2 != nil {
@@ -35,16 +47,52 @@ func handle() (string, error) {
 	return "process complete!", nil
 }
 
+// invalidationのInputを作成
+func makeInvalidate(distributionId string, paths []*string) (cloudfront.CreateInvalidationInput, error) {
+
+	// 削除の指定がない場合全件削除を追加
+	if len(paths) == 0 {
+		stab := "/*"
+		paths = append(paths, &stab)
+	}
+	pathsLength := int64(len(paths))
+	deleteItems := cloudfront.Paths{
+		Items:    paths,
+		Quantity: &pathsLength,
+	}
+
+	timeStamp := strconv.FormatInt(time.Now().Unix(), 10)
+	invalidationBatch := cloudfront.InvalidationBatch{
+		CallerReference: &timeStamp,
+		Paths:           &deleteItems,
+	}
+
+	invalidationInput := cloudfront.CreateInvalidationInput{
+		DistributionId:    &distributionId,
+		InvalidationBatch: &invalidationBatch,
+	}
+
+	return invalidationInput, nil
+}
+
 // キャッシュ削除処理
 // 削除完了は待てないので、実行だけ。
-func doInvalidate() bool {
-	return false
+func doInvalidate(input cloudfront.CreateInvalidationInput) (*cloudfront.CreateInvalidationOutput, error) {
+	// Lambdaに実行権限を付与するのでClient情報はいらない
+	client := cloudfront.CloudFront{
+		Client: nil,
+	}
+	output, invalidateError := client.CreateInvalidation(&input)
+
+	return output, invalidateError
 }
 
 // slackに連絡入れる
 func postSlack(message string, status bool) error {
 	webHookURL := os.Getenv("SLACK_WEB_HOOK_URL")
 	iconUrl := os.Getenv("SLACK_ICON_URL")
+
+	// messageが空文字の場合、固定のキャッシュ削除実行有無を指す文言をセットする
 	if len(message) == 0 {
 		message = map[bool]string{
 			true:  "キャッシュ削除中\nそのうち削除されるのでしばしお待ちを",
